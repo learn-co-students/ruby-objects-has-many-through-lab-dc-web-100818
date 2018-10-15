11f9106ac0adfb3c131afb8c1e2d27ba268fad60
Ana Harris <vana1990@gmail.com>
2018-10-14 20:37:27 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name, :artist
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song (name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    songs.collect {|song| song.genre}
+  end
+
+end
